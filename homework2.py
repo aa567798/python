67e0cdd90c2224c6d9038871fa59a4671aafb54e
aa567798 <s091184@yahoo.com.tw>
2015-10-26 15:16:29 +0800

    1026

---
+++ homework2.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[2]:
+
+60*60
+
+
+# In[4]:
+
+seconds_per_hour=3600
+print(seconds_per_hour*24)
+
+
+# In[5]:
+
+seconds_per_day=86400
+seconds_per_day/seconds_per_hour
+
+
+# In[6]:
+
+seconds_per_day//seconds_per_hour
+
+
+# In[ ]:
+
+
+
